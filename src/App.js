efa4d8f0a4275488202501a6c647f1c68efa754f
salemsmara <salemsmara@gmail.com>
2022-06-13 17:13:28 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import imge from "./profilimage.png"
+class App extends Component {
+  //src\imgprostate.png
+  constructor(props) {
+    super(props);
+    this.state ={
+      personne: {
+        fullName: "salem",
+        bio: "etudiant a centre de formation go my code 2022 / etudiant a ISIGK 2013---2016",
+        imgsrs: "imge",
+        profession: "developpeur web full stack",
+      },
+      show: false,
+      count : 0
+    }
+    }
+    Afficher =()=>{this.setState({show:!this.state.show})};
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    componentDidMount() { 
+      setInterval(()=>{
+         this.setState({count:this.state.count+1})
+       }, 500);
+      }
+
+    
+  render() {
+    return (
+      <div >
+    <p>  {this.state.count}</p>
+
+    {this.state.show ? <img src={imge} style= {{width:'180px' }}alt="pucture "/> : null}
+
+    {this.state.show ? <p> {this.state.personne.fullName} </p>: null}
+    {this.state.show ? <p> {this.state.personne.bio} </p>: null}
+    {this.state.show ? <p> {this.state.personne.profession} </p>: null}
+
+      <button onClick={this.Afficher}>information</button>
+      
+      </div>
+      )
+    
+    
+  }
 }
-
 export default App;
